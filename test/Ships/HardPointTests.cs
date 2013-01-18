3379c90ab475ff77c5ed83671cc831ccc58075bb
Andy Clarke <andy.uq@gmail.com>
2013-01-18 17:07:20 +1300

    Hard points calculate based on ship heading

--- test/Ships/HardPointTests.cs
+++ test/Ships/HardPointTests.cs
@@ -12,7 +12,7 @@ namespace Echo.Tests.Ships
 		private readonly Ship _frontTarget = new Ship() { Position = new Position(new Universe(), new Vector(0, 1, 0)) };
 		private readonly Ship _rearTarget = new Ship() { Position = new Position(new Universe(), new Vector(0, -1, 0)) };
 		private readonly Ship _sideTarget = new Ship() { Position = new Position(new Universe(), new Vector(-1, 1, 0)) };
-		private Ship _ship = new Ship();
+		private Ship _ship;
 
 		private class CanTrackResult
 		{
@@ -21,6 +21,12 @@ namespace Echo.Tests.Ships
 			public bool Front { get; set; }
 			public bool Rear { get; set; }
 			public bool LeftTop { get; set; }
+		}
+
+		[SetUp]
+		public void SetUpShip()
+		{
+			_ship = new Ship();
 		}
 
 		[TestCase(HardPointPosition.Left, "0,1", "-1,0")]
@@ -59,6 +65,37 @@ namespace Echo.Tests.Ships
 		}
 
 		[Test]
+		public void CanTrackWithShipHeading()
+		{
+			var headingRight = new Vector(1,0);
+
+			CanTrack(headingRight, HardPointPosition.Left, new CanTrackResult() { Front = true, LeftTop = true });
+			CanTrack(headingRight, HardPointPosition.Right, new CanTrackResult() { Rear = true });
+			CanTrack(headingRight, HardPointPosition.Top, new CanTrackResult() { Left = true, Right = true, Front = true, Rear = true, LeftTop = true, });
+			CanTrack(headingRight, HardPointPosition.Bottom, new CanTrackResult() { Left = true, Right = true, Front = true, Rear = true, LeftTop = true });
+			CanTrack(headingRight, HardPointPosition.Front, new CanTrackResult() { Right = true, Rear = true, Front = true });
+			CanTrack(headingRight, HardPointPosition.Rear, new CanTrackResult() { Front = true, Left = true, LeftTop = true, Rear = true });
+
+			var headingLeft = new Vector(-1,0);
+
+			CanTrack(headingLeft, HardPointPosition.Left, new CanTrackResult() { Rear = true });
+			CanTrack(headingLeft, HardPointPosition.Right, new CanTrackResult() { Front = true, LeftTop = true });
+			CanTrack(headingLeft, HardPointPosition.Top, new CanTrackResult() { Left = true, Right = true, Front = true, Rear = true, LeftTop = true, });
+			CanTrack(headingLeft, HardPointPosition.Bottom, new CanTrackResult() { Left = true, Right = true, Front = true, Rear = true, LeftTop = true });
+			CanTrack(headingLeft, HardPointPosition.Front, new CanTrackResult() { Front = true, Left = true, LeftTop = true, Rear = true });
+			CanTrack(headingLeft, HardPointPosition.Rear, new CanTrackResult() { Right = true, Rear = true, Front = true });
+
+			var headingDown = new Vector(0,-1);
+
+			CanTrack(headingDown, HardPointPosition.Left, new CanTrackResult() { Right = true });
+			CanTrack(headingDown, HardPointPosition.Right, new CanTrackResult() { Left = true, LeftTop = true });
+			CanTrack(headingDown, HardPointPosition.Top, new CanTrackResult() { Left = true, Right = true, Front = true, Rear = true, LeftTop = true, });
+			CanTrack(headingDown, HardPointPosition.Bottom, new CanTrackResult() { Left = true, Right = true, Front = true, Rear = true, LeftTop = true });
+			CanTrack(headingDown, HardPointPosition.Front, new CanTrackResult() { Rear = true, Left = true, Right = true });
+			CanTrack(headingDown, HardPointPosition.Rear, new CanTrackResult() { Right = true, Front = true, LeftTop = true, Left = true });
+		}
+
+		[Test]
 		public void Aims()
 		{
 			var hp = new HardPoint(_ship, HardPointPosition.Top);
@@ -83,6 +120,12 @@ namespace Echo.Tests.Ships
 			Assert.That(hp.CanTrack(_frontTarget), Is.EqualTo(expected.Front), "Can track front");
 			Assert.That(hp.CanTrack(_rearTarget), Is.EqualTo(expected.Rear), "Can track rear");
 			Assert.That(hp.CanTrack(_sideTarget), Is.EqualTo(expected.LeftTop), "Can track left-top");
+		}
+
+		private void CanTrack(Vector shipHeading, HardPointPosition position, CanTrackResult expected)
+		{
+			_ship.Heading = shipHeading;
+			CanTrack(position, expected);
 		}
 	}
 }
