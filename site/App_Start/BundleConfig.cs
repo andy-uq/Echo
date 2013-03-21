1c76546c04e57d278860b194598ed105ca860485
Andy Clarke <andy.uq@gmail.com>
2013-03-21 22:35:33 +1300

    Styling

--- site/App_Start/BundleConfig.cs
+++ site/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Echo.Web
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Main.css"));
 
 			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
 						"~/Content/themes/base/jquery.ui.core.css",
