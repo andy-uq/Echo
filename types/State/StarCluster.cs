eb7f67f4eead4bf07ac5fcbdc9e7ae8f540c2d65
Andy Clarke <andy.uq@gmail.com>
2013-03-22 20:34:04 +1300

    Default constructor

--- types/State/StarCluster.cs
+++ types/State/StarCluster.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Echo.State
 {
 	public class StarClusterState : IObjectState
 	{
+		public StarClusterState()
+		{
+			SolarSystems = Enumerable.Empty<SolarSystemState>();
+		}
+
 		public string Id { get; set; }
 		public long ObjectId { get; set; }
 		public string Name { get; set; }
