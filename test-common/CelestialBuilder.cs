bd08d75d1434947b362f6868e72424d755d40cc3
Andy Clarke <andy.uq@gmail.com>
2013-07-13 10:23:28 +1200

    Add object builder for tests

---
+++ test-common/CelestialBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Echo;
+using Echo.State;
+
+namespace test.common
+{
+	public class CelestialBuilder
+	{
+		public SolarSystemBuilder SolarSystem()
+		{
+			return new SolarSystemBuilder();
+		}
+
+		public SolarSystemState SolarSystem(Action<SolarSystemBuilder> builder)
+		{
+			var solarSystem = SolarSystem();
+			builder(solarSystem);
+
+			return solarSystem.Build();
+		}
+	}
+
+	public class SolarSystemBuilder : CelestialBuilder
+	{
+		private readonly SolarSystemState _solarSystem;
+		private List<CelestialObjectState> _satellites;
+
+		public SolarSystemBuilder()
+		{
+			_solarSystem = new SolarSystemState();
+		}
+
+		public SolarSystemBuilder Planet(Vector localCoordinates = default(Vector))
+		{
+			var planet = new CelestialObjectState
+			{
+				LocalCoordinates = localCoordinates,
+			};
+
+			_satellites.Add(planet);
+			return this;
+		}
+
+		public SolarSystemState Build()
+		{
+			return new SolarSystemState
+			{
+				Satellites = new System.Collections.Generic.,
+			};
+		}
+	}
+}
---
+++ test-common/ObjectFactory.cs
@@ -0,0 +1,7 @@
+namespace test.common
+{
+	public static class ObjectFactory
+	{
+		public static CelestialBuilder Celestial { get { return new CelestialBuilder(); } }
+	}
+}
